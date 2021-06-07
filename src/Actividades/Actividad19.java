c91e03dc2bdda4e9ad6e19891806a16043428c7b
Georgie <gbenevento@cifpfbmoll.eu>
2021-06-07 11:57:58 +0200

    18 y 19

--- src/Actividades/Actividad19.java
+++ src/Actividades/Actividad19.java
@@ -3,8 +3,7 @@ package Actividades;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Actividad18 {
-
+public class Actividad19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Input the size of the first vector");
@@ -21,15 +20,24 @@ public class Actividad18 {
             System.out.println("Input v2 value, one by one");
             v2[i] = sc.nextInt();
         }
-
-        int[] v3 = new int[v1size + v2size];
-        for (int i = 0; i < v1size; i++) {
-            v3[i] = v1[i];
-        }
-        for (int i = v1size; i < v3.length; i++) {
-            v3[i] = v2[i - v1size];
+        int[] v3;
+        if (v1size > v2size) {
+            v3 = new int[v1size];
+            for (int i = 0; i < v2size; i++) {
+                v3[i] = v1[i] + v2[i];
+            }
+            for (int i = v2size; i < v3.length; i++) {
+                v3[i] = v1[i];
+            }
+        } else {
+            v3 = new int[v2size];
+            for (int i = 0; i < v2size; i++) {
+                v3[i] = v1[i] + v2[i];
+            }
+            for (int i = v2size; i < v3.length; i++) {
+                v3[i] = v1[i];
+            }
         }
         System.out.println(Arrays.toString(v3));
     }
-
 }
